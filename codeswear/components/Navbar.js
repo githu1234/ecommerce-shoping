a55ac3f3f477fcda1564d521dcf0588fbf51bf8b
githu1234 <sathish.maadu@gmail.com>
2022-04-11 18:36:48 +0530

    mangoose-installed&model-folder-created

--- codeswear/components/Navbar.js
+++ codeswear/components/Navbar.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import  { useRef } from "react";
 import { AiOutlineShoppingCart,AiFillCloseCircle,AiFillPlusCircle,AiFillMinusCircle} from 'react-icons/ai';
 import {BsFillBagCheckFill} from 'react-icons/bs'
 import {MdAccountCircle} from 'react-icons/md'
-
-import { useRef } from 'react'
 import '../pages/_app.js'
 
 const Navbar = ({cart,addToCart,removeFromCart,clearCart,subTotal}) => {
@@ -27,11 +26,9 @@ const Navbar = ({cart,addToCart,removeFromCart,clearCart,subTotal}) => {
   return (
     <div className='flex flex-col py-4 sticky top-0 md:flex-row md:justify-start justify-center items-center my-0 shadow-xl bg-white z-10'>
       <div className="logo mx-5"> 
-      <Link href={"/"}>
-       <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+      <Link href={"/"} passHref><a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
        <Image width={200} height={40} src="/footer.png"></Image>
-        </a>
-       </Link>
+        </a></Link>
 
       </div>
       <div className='nav'>
@@ -43,7 +40,7 @@ const Navbar = ({cart,addToCart,removeFromCart,clearCart,subTotal}) => {
         </ul>
       </div>
       <div  className="cart absolute right-0 top-2 mx-5 cursor-pointer flex ">
-        <Link href={'/login'}><MdAccountCircle  className=' text-xl md:text-3xl'/></Link>
+        <Link href={'/login'}><MdAccountCircle  className=' text-xl md:text-3xl mx-4'/></Link>
         <AiOutlineShoppingCart onClick={toggleCart} className=' text-xl md:text-3xl'/>
       </div>
       <div ref={ref} className={`w-72 h-[100vh] sideCart absolute top-0 right-0 bg-pink-50 px-8 py-10 transform transition-transform ${Object.keys(cart).length!==0?'translate-x-0':'translate-x-full'}`}>
---
+++ codeswear/models/Order.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+const OrderSchema=new mongoose.Schema({
+  userId:{type: String,required: true},
+  products:[{
+    productId:{type:String},
+    quantity:{type:Number,default:1}
+  }],
+  address:{type:String,requrired:true},
+  amount:{type:number,required:true},
+  status:{type:String,default:pending,required:true},
+},{timestamps:true});
+export default mongoose.model("Order",OrderSchema)
